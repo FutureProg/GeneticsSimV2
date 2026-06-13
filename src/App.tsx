d11d013ed1ab3fc0391bc12fc9b14d520cd24e5f
Nick Morrison <nickmorrison09@gmail.com>
2026-06-13 15:08:37 -0400

    Added blob selection logic

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ type BlobData = {
   y: number;
   dirX: number;
   dirY: number;
+  selected: boolean;
 };
 
 const BLOB_SIZE = 100 as const;
@@ -20,6 +21,18 @@ function App() {
   const bounds = useRef<{ width: number; height: number }>({ width: window.innerWidth, height: window.innerHeight });
   const resizeObserver = useRef<ResizeObserver>(null);
 
+  const onBlobClick = (blob: BlobData) => {
+    const selectedBlobs = blobsArr.current.filter(b => b.selected);
+    if (selectedBlobs.length >= 2 && !blob.selected) {
+      selectedBlobs.forEach(b => {
+        b.selected = false;
+        b.element.classList.remove('selected');
+      });
+    }
+    blob.selected = !blob.selected;
+    blob.element.classList.toggle('selected', blob.selected);        
+  }
+
   const initBlobs = () => {
     bounds.current = { width: blobsPool.current?.clientWidth || window.innerWidth, height: blobsPool.current?.clientHeight || window.innerHeight };
     blobsArr.current = Array.from(document.querySelectorAll<SVGSVGElement>('.blob')).map(element => ({
@@ -27,12 +40,14 @@ function App() {
       x: Math.random() * (bounds.current.width - BLOB_SIZE),
       y: Math.random() * (bounds.current.height - BLOB_SIZE),
       dirX: Math.random() * 2 - 1,
-      dirY: Math.random() * 2 - 1
+      dirY: Math.random() * 2 - 1,
+      selected: false,
     }));
     blobsArr.current.forEach(blob => {
       blob.element.style.transform = `translate(${blob.x}px, ${blob.y}px)`;
+      blob.element.onclick = onBlobClick.bind(null, blob);
     });
-  }
+  }  
 
   const collisionDetection = (blobA: BlobData, blobB: BlobData) => {
     const radius = BLOB_SIZE / 2;
@@ -66,8 +81,9 @@ function App() {
       for (let j = i + 1; j < blobs.length; j++)
         collisionDetection(blobs[i], blobs[j]);
 
-    blobsArr.current = blobsArr.current.map(blob => {
+    blobsArr.current.forEach(blob => {
       const { element, x, y } = blob;
+      if (blob.selected) return;    
       if (x <= 0 && blob.dirX < 0) blob.dirX *= -1;
       if (x >= bounds.current.width - BLOB_SIZE && blob.dirX > 0) blob.dirX *= -1;
       if (y <= 0 && blob.dirY < 0) blob.dirY *= -1;
@@ -78,8 +94,7 @@ function App() {
       blob.x = nx;
       blob.y = ny;
       element.style.transform = `translate(${nx}px, ${ny}px)`;
-      return blob;
-    });
+});
     requestAnimationFrame(render);
   }
 
